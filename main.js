99491b293514eba781dfff47b03c8c37fd53a505
Keith Potempa <keithrpotempa@gmail.com>
2020-02-10 16:05:44 -0600

    challenge complete

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 import salesByWeek from './data.js'
 
-/*
-[x] Display the first and last name of the sales agent.
-[x] Display all keys and values for the car sold.
-[x] Display the gross profit made on the sale.
-*/
-const agents = []
-
 const salesReportContainer = document.getElementById("salesReport__container")
 salesReportContainer.innerHTML = "<h1>Weekly Sales Report</h1><hr>"
 
@@ -28,12 +21,39 @@ const agentReportFactory = (sale) => {
     `
 }
 
-const carSaleFactory = (car) => {
-    return `
-    `
-}
-
 for (const sale of Object.values(salesByWeek)) {
-    // console.log("sale =", sale)
     salesReportContainer.innerHTML += agentReportFactory(sale);
 }
+
+/*
+[ ] Your first task is to use object methods 
+to match the value of all properties of the sales_agent object 
+in each sale against the input from Randall. 
+
+[x] Put an input field in your DOM, 
+[x] attach a keypress event listener to it, 
+[x] and search when Randall presses enter. 
+*/
+
+const searchInput = document.querySelector("#searchInput")
+console.log(searchInput)
+searchInput.addEventListener('keypress', event => {
+    if (event.charCode === 13) {
+        const searchTerm = event.target.value
+        const agentSales = salesByWeek.filter(sale => {
+            let filter = false
+            let agent = sale.sales_agent;
+            for (const prop of Object.values(agent)) {
+                if (prop.includes(searchTerm)) {
+                    console.log(prop, "contains", searchTerm)
+                    filter = true
+                }
+            }
+            return filter
+        })
+        salesReportContainer.innerHTML = "<h1>Search Results</h1><hr>"
+        agentSales.forEach(sale => {
+            salesReportContainer.innerHTML += agentReportFactory(sale);
+        })
+    }
+});
